89a26326dd2018939ecd2a5d93c3c09e81abbdfd
herewithme <amaury@beapi.fr>
2012-11-06 08:49:09 +0100

    first commit, big work on readme !

--- README.md
+++ README.md
@@ -1,4 +1,69 @@
-wp-filters-extras
+WP Filters Extras
 =================
 
-Add 2 methods for WP Filter API
+Add 2 PHP functions for WP Filter API, it's allow to remove filter/action on hooks for specific cases with CLASS PHP.
+
+**These functions are very useful for removing filters and actions from plugin not very well developed!**
+
+## Remove filters with method name only 
+
+`remove_filters_with_method_name( $hook_name = '', $method_name = '', $priority = 0 );`
+
+Allow to remove method for an hook when, it's a class method used and class don't have global for instanciation !
+
+## Remove filters with class and method name
+
+ `remove_filters_for_anonymous_class( $hook_name = '', $class_name ='', $method_name = '', $priority = 0 );`
+
+Allow to remove method for an hook when, it's a class method used and class don't have global for instanciation, but you know the class name :)
+
+## Usage
+
+### Example :
+	
+	// First sample
+	class MyClassA {
+	    function __construct() {
+	        add_action( 'wp_footer', array( $this, 'my_action' ), 10 );
+	    }
+	    function my_action() {
+	        print '<h1>' . __class__ . ' - ' . __function__ . '</h1>';
+	    }
+	}
+	new MyClassA();
+
+	// Second sample
+	class MyClassB {
+	    function __construct() {
+	        add_action( 'wp_footer', array( $this, 'my_action' ), 10 );
+	    }
+	    function my_action() {
+	        print '<h1>' . __class__ . ' - ' . __function__ . '</h1>';
+	    }
+	}
+	new MyClassB();
+
+### The problem
+
+With WordPress API, you can't remove the filter `my_action` because MyClassA and MyClassB don't have a variable !
+The right way to load class is : `$my_class_b = new MyClassB();`
+
+### Way 1
+
+This first method, `remove_filters_with_method_name();`, is fairly aggressive because it removes all filters that have the name "my_action", whatever the class.
+
+	remove_filters_with_method_name( 'wp_footer', 'my_action', 10 );
+
+This call will remove the 2 filters for classes MyClassA and MyClassB.
+
+### Way 2
+
+The second method, `remove_filters_for_anonymous_class();`, is more accurate because it is necessary to specify both the name of the method but also the name of the PHP class.
+
+	remove_filters_for_anonymous_class( 'wp_footer', 'MyClassB', my_action', 10 );
+
+This call will only remove the filter for the class MyClassB.
+
+## Tips
+
+The priority argument of the two functions is important, you must use the same priority as the hook you want to remove !
---
+++ wp-filters-extras.php
@@ -0,0 +1,74 @@
+<?php
+/*
+Plugin Name: WP Filters Extras
+Plugin URI: http://www.beapi.fr
+Description: Add 2 methods for WP Filter API
+Version: 1.0
+Author: BeAPI
+Author URI: http://www.beapi.fr
+
+Copyright 2012 Amaury Balmer - amaury@beapi.fr
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License, version 2, as 
+published by the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+/**
+ * Allow to remove method for an hook when, it's a class method used and class don't have global for instanciation !
+ */
+function remove_filters_with_method_name( $hook_name = '', $method_name = '', $priority = 0 ) {
+	global $wp_filter;
+	
+	// Take only filters on right hook name and priority
+	if ( !isset($wp_filter[$hook_name][$priority]) || !is_array($wp_filter[$hook_name][$priority]) )
+		return false;
+	
+	// Loop on filters registered
+	foreach( (array) $wp_filter[$hook_name][$priority] as $unique_id => $filter_array ) {
+		// Test if filter is an array ! (always for class/method)
+		if ( isset($filter_array['function']) && is_array($filter_array['function']) ) {
+			// Test if object is a class and method is equal to param !
+			if ( get_class($filter_array['function'][0]) && $filter_array['function'][1] == $method_name ) {
+				unset($wp_filter[$hook_name][$priority][$unique_id]);
+			}
+		}
+		
+	}
+	
+	return false;
+}
+
+/**
+ * Allow to remove method for an hook when, it's a class method used and class don't have variable, but you know the class name :)
+ */
+function remove_filters_for_anonymous_class( $hook_name = '', $class_name ='', $method_name = '', $priority = 0 ) {
+	global $wp_filter;
+	
+	// Take only filters on right hook name and priority
+	if ( !isset($wp_filter[$hook_name][$priority]) || !is_array($wp_filter[$hook_name][$priority]) )
+		return false;
+	
+	// Loop on filters registered
+	foreach( (array) $wp_filter[$hook_name][$priority] as $unique_id => $filter_array ) {
+		// Test if filter is an array ! (always for class/method)
+		if ( isset($filter_array['function']) && is_array($filter_array['function']) ) {
+			// Test if object is a class, class and method is equal to param !
+			if ( get_class($filter_array['function'][0]) && get_class($filter_array['function'][0]) == $class_name && $filter_array['function'][1] == $method_name ) {
+				unset($wp_filter[$hook_name][$priority][$unique_id]);
+			}
+		}
+		
+	}
+	
+	return false;
+}
