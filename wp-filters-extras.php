0a1890131ef839e600f3005669efef615344a773
Julien Maury <jmaury@beapi.fr>
2017-10-02 22:04:01 +0200

    bump version

--- wp-filters-extras.php
+++ wp-filters-extras.php
@@ -3,11 +3,11 @@
 Plugin Name: WP Filters Extras
 Plugin URI: http://www.beapi.fr
 Description: Add 2 methods for WP Filter API
-Version: 1.0.1
+Version: 1.0.2
 Author: BeAPI
 Author URI: http://www.beapi.fr
 
-Copyright 2012 Amaury Balmer - amaury@beapi.fr
+Copyright 2012-2017 Amaury Balmer - amaury@beapi.fr
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License, version 2, as
@@ -30,22 +30,22 @@ function remove_filters_with_method_name( $hook_name = '', $method_name = '', $p
 	global $wp_filter;
 
 	// Take only filters on right hook name and priority
-	if ( !isset($wp_filter[$hook_name][$priority]) || !is_array($wp_filter[$hook_name][$priority]) )
+	if ( ! isset( $wp_filter[ $hook_name ][ $priority ] ) || ! is_array( $wp_filter[ $hook_name ][ $priority ] ) ) {
 		return false;
+	}
 
 	// Loop on filters registered
-	foreach( (array) $wp_filter[$hook_name][$priority] as $unique_id => $filter_array ) {
+	foreach ( (array) $wp_filter[ $hook_name ][ $priority ] as $unique_id => $filter_array ) {
 		// Test if filter is an array ! (always for class/method)
-		if ( isset($filter_array['function']) && is_array($filter_array['function']) ) {
+		if ( isset( $filter_array['function'] ) && is_array( $filter_array['function'] ) ) {
 			// Test if object is a class and method is equal to param !
-			if ( is_object($filter_array['function'][0]) && get_class($filter_array['function'][0]) && $filter_array['function'][1] == $method_name ) {
-			    // Test for WordPress >= 4.7 WP_Hook class (https://make.wordpress.org/core/2016/09/08/wp_hook-next-generation-actions-and-filters/)
-			    if( is_a( $wp_filter[$hook_name], 'WP_Hook' ) ) {
-			        unset( $wp_filter[$hook_name]->callbacks[$priority][$unique_id] );
-			    }
-			    else {
-				    unset($wp_filter[$hook_name][$priority][$unique_id]);
-			    }
+			if ( is_object( $filter_array['function'][0] ) && get_class( $filter_array['function'][0] ) && $filter_array['function'][1] == $method_name ) {
+				// Test for WordPress >= 4.7 WP_Hook class (https://make.wordpress.org/core/2016/09/08/wp_hook-next-generation-actions-and-filters/)
+				if ( is_a( $wp_filter[ $hook_name ], 'WP_Hook' ) ) {
+					unset( $wp_filter[ $hook_name ]->callbacks[ $priority ][ $unique_id ] );
+				} else {
+					unset( $wp_filter[ $hook_name ][ $priority ][ $unique_id ] );
+				}
 			}
 		}
 
@@ -57,26 +57,26 @@ function remove_filters_with_method_name( $hook_name = '', $method_name = '', $p
 /**
  * Allow to remove method for an hook when, it's a class method used and class don't have variable, but you know the class name :)
  */
-function remove_filters_for_anonymous_class( $hook_name = '', $class_name ='', $method_name = '', $priority = 0 ) {
+function remove_filters_for_anonymous_class( $hook_name = '', $class_name = '', $method_name = '', $priority = 0 ) {
 	global $wp_filter;
 
 	// Take only filters on right hook name and priority
-	if ( !isset($wp_filter[$hook_name][$priority]) || !is_array($wp_filter[$hook_name][$priority]) )
+	if ( ! isset( $wp_filter[ $hook_name ][ $priority ] ) || ! is_array( $wp_filter[ $hook_name ][ $priority ] ) ) {
 		return false;
+	}
 
 	// Loop on filters registered
-	foreach( (array) $wp_filter[$hook_name][$priority] as $unique_id => $filter_array ) {
+	foreach ( (array) $wp_filter[ $hook_name ][ $priority ] as $unique_id => $filter_array ) {
 		// Test if filter is an array ! (always for class/method)
-		if ( isset($filter_array['function']) && is_array($filter_array['function']) ) {
+		if ( isset( $filter_array['function'] ) && is_array( $filter_array['function'] ) ) {
 			// Test if object is a class, class and method is equal to param !
-			if ( is_object($filter_array['function'][0]) && get_class($filter_array['function'][0]) && get_class($filter_array['function'][0]) == $class_name && $filter_array['function'][1] == $method_name ) {
-			    // Test for WordPress >= 4.7 WP_Hook class (https://make.wordpress.org/core/2016/09/08/wp_hook-next-generation-actions-and-filters/)
-			    if( is_a( $wp_filter[$hook_name], 'WP_Hook' ) ) {
-			        unset( $wp_filter[$hook_name]->callbacks[$priority][$unique_id] );
-			    }
-			    else {
-				    unset($wp_filter[$hook_name][$priority][$unique_id]);
-			    }
+			if ( is_object( $filter_array['function'][0] ) && get_class( $filter_array['function'][0] ) && get_class( $filter_array['function'][0] ) == $class_name && $filter_array['function'][1] == $method_name ) {
+				// Test for WordPress >= 4.7 WP_Hook class (https://make.wordpress.org/core/2016/09/08/wp_hook-next-generation-actions-and-filters/)
+				if ( is_a( $wp_filter[ $hook_name ], 'WP_Hook' ) ) {
+					unset( $wp_filter[ $hook_name ]->callbacks[ $priority ][ $unique_id ] );
+				} else {
+					unset( $wp_filter[ $hook_name ][ $priority ][ $unique_id ] );
+				}
 			}
 		}
 
